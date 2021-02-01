343788a3103511dd5f256df5ced0d5295e5204e9
Rehack65 <dojojesse@gmail.com>
2021-02-01 10:16:16 -0500

    Added reset values to replay button and updated readme

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Project Description
 
 The-Very-Best is a rock paper scissors game using pokemone types, but you're shown what the oponent will chose.
+Example: A grass type pokemon would beat a water type pokemon.
 
 ## API and Data Sample
 
@@ -44,7 +45,7 @@ https://wireframe.cc/bydHV9
 
 - Add some background battle music.
 - Display pokemon win/loss condtions for this match.
-- Add dynamic health-bars to the pokemon to indicate which one one and loss by depletion.
+- Add replay button
 - Keep track of and display amount of wins and losses based on comparision outcomes.
 
 ## Project Schedule
@@ -52,9 +53,9 @@ https://wireframe.cc/bydHV9
 |---|---| ---|
 |Jan 25-26| Prompt / Wireframes / Priority Matrix / Timeframes | Completed
 |Jan 26| Project Approval and PseudoCoding | Completed
-|Jan 27| Core Application Structure (HTML, CSS, etc.) | Incomplete
-|Jan 28| Initial Clickable Model  | Incomplete
-|Jan 29| MVP | Incomplete
+|Jan 27| Core Application Structure (HTML, CSS, etc.) | Completed
+|Jan 29| Initial Clickable Model  | Completed
+|Jan 30| MVP | Completed
 |Feb 1| Presentations/Project Submission | Incomplete
 
 ## Priority Matrix
@@ -66,29 +67,33 @@ https://wireframe.cc/bydHV9
 | Component | Priority | Estimated Time | Actual Time |
 | --- | :---: |  :---: | :---: |
 | Boilerplate HTML and CSS | H | 2hrs| 1hr |
-| Take user input and display name | H | 3hrs| |
-| Take user input to select pokemon | H | 3hrs| |
-| Randomly generate opposing pokemon | H | 3hrs| |
-| Display/layout pokemon sprites | H | 3hrs| |
-| Get type match up data for each pokemon | H | 3hrs| |
-| Create win conditions for battle | H | 3hrs| |
-| Display winner from battle | H | 3hrs| |
-| Request API (for each pokemon) to get name and sprite data | H | 3hrs| |
-| Request API (for each pokemon) to get type match up data | H | 3hrs| |
-| Background and format styling | H | 3hrs| |
-| Debugging | H | 4hrs| |
+| Take user input and display name | H | 3hrs| 3hrs|
+| Take user input to select pokemon | H | 3hrs| 3hrs|
+| Randomly generate opposing pokemon | H | 3hrs| 2hrs|
+| Display/layout pokemon sprites | H | 3hrs| 4hrs|
+| Get type match up data for each pokemon | H | 3hrs| 3hrs|
+| Create win conditions for battle | H | 3hrs| 3hrs|
+| Display winner from battle | H | 3hrs| 5hrs |
+| Request API (for each pokemon) to get name and sprite data | H | 3hrs| 4hrs|
+| Request API (for each pokemon) to get type match up data | H | 3hrs| 3hrs|
+| Background and format styling | H | 3hrs|3hrs |
+| Debugging | H | 4hrs| 2hrs|
 | Post MVP + Research | H | 7hrs| |
-| Total | H | 34hrs| |
+| Total | H | 34hrs| 36Hrs |
 
 ## Code Snippet
-
-Use this section to include a brief code snippet of functionality that you are proud of and a brief description.  
-
 ```
-function reverse(string) {
-	// here is the code to reverse a string of text
-}
+// Getting User name submittion and adding event listener ===============================
+let nameSubmit = document.getElementById('user-name')
+nameSubmit.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let userName = document.getElementById('name').value
+  // Replacing the username form with the user Pokemon form =============================
+  document.getElementById('user-name').style.display = "none"
+  document.getElementById('user-pokemon-name').style.display = "block"
+  let battleText = document.getElementById('battle-text1')
+  battleText.innerText = `Welcome ${userName}. Team rocket has stolen all Pokemon's second typing!`
+});`
 ```
-
 ## Change Log
- Use this section to document what changes were made and the reasoning behind those changes.  
+ 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Getting random Pokemon information and appeneding sprite/name/type =======================
+// Getting random Pokemon information and appeneding sprite/name/type ====================
 async function randomPokemon(num) {
   try {
     const opposingPokeData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}/`)
@@ -6,7 +6,7 @@ async function randomPokemon(num) {
     let opposingSpriteData = opposingPokeData.data.sprites.front_default
     let opposingTypeData = opposingPokeData.data.types[0].type.name
     let opposingPokemonName = document.getElementById('opposing-pokemon-name');
-    // Formatting name so its first letter is uppercase
+    // Formatting name so its first letter is uppercase =================================
     opposingPokemonName.append(opposingNameData.charAt(0).toUpperCase() + opposingNameData.slice(1))
     opposingPokemonName.append(` ${opposingTypeData}` + ' type')
     let opposingSprite = document.getElementById('opposing-pokemon-sprite');
@@ -18,8 +18,7 @@ async function randomPokemon(num) {
   }
 }
 
-
-// Evoking random Pokemon
+// Evoking random Pokemon ===============================================================
 randomPokemon(Math.floor(Math.random() * 898) + 1)
 
 // Getting User name submittion and adding event listener ===============================
@@ -27,13 +26,14 @@ let nameSubmit = document.getElementById('user-name')
 nameSubmit.addEventListener("submit", function (e) {
   e.preventDefault();
   let userName = document.getElementById('name').value
-  // Replacing the username form with the user Pokemon form ============================
+  // Replacing the username form with the user Pokemon form =============================
   document.getElementById('user-name').style.display = "none"
   document.getElementById('user-pokemon-name').style.display = "block"
   let battleText = document.getElementById('battle-text1')
   battleText.innerText = `Welcome ${userName}. Team rocket has stolen all Pokemon's second typing!`
 });
-// Getting user Pokemon submition and adding event listener for form
+
+// Getting user Pokemon submition and adding event listener for form ===============
 let pokemonSubmit = document.getElementById('user-pokemon-name')
 pokemonSubmit.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -42,29 +42,32 @@ pokemonSubmit.addEventListener("submit", function (e) {
   document.getElementById('replay-button').style.display = "block"
   document.getElementById('user-pokemon-sprite').style.display = "block"
 });
-// Adding the last event listener to replay the game
+
+// Adding the last event listener to replay the game and reset some values ========================
 let replayButton = document.getElementById('replay-button')
 replayButton.addEventListener('click', function (e) {
   document.getElementById('opposing-pokemon-name').innerText = ''
+  document.getElementById('user-pokemon').value = ''
+  document.getElementById('user-pokemon-sprite').style.display = "none"
   randomPokemon(Math.floor(Math.random() * 898) + 1)
 })
+
+// Getting user input Pokemon sprite and type data
 async function UserPokemon(input) {
   try {
     const userPokeData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}/`)
-    let userNameData = input.charAt(0).toUpperCase() + input.slice(1)
     let userSpriteData = userPokeData.data.sprites.back_default
     let userTypeData = userPokeData.data.types[0].type.url
-    let userPokemonName = document.getElementsByClassName('user-pokemon-name');
-    // userPokemonName.p = userNameData
     let userSprite = document.getElementById('user-pokemon-sprite')
     userSprite.src = userSpriteData
+    // Main comparison between user Pokemon type and super effectiveness vs opposing Pokemon typing
     async function Compare() {
       try {
         const typeTable = await axios.get(`${userTypeData}`)
         let winCondition = typeTable.data.damage_relations.double_damage_to
-        //  console.log(winCondition);
         let opposingPokemonName = document.getElementById('opposing-pokemon-name').innerText;
         let opposingPokeType = opposingPokemonName.split(" ")[1]
+        // Setting and running win condition
         for (let i = 0; i <= winCondition.length; i++) {
           let WinIndex = winCondition[i];
           if (WinIndex.name === opposingPokeType) {
